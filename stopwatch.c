79583431b3a614e65b4ef6573b876b8531785027
Orson Peters <spionman007@gmail.com>
2012-02-19 01:32:48 +0100

    stopwatch update

--- stopwatch.c
+++ stopwatch.c
@@ -1,60 +1,57 @@
+/*
+
+    Copyright 2011 Orson Peters. All rights reserved.
+
+    Redistribution of this work, with or without modification, is permitted if
+    Orson Peters is attributed as the original author or licensor of
+    this work, but not in any way that suggests that Orson Peters endorses
+    you or your use of the work.
+
+    This work is provided by Orson Peters "as is" and any express or implied
+    warranties are disclaimed. Orson Peters is not liable for any damage
+    arising in any way out of the use of this work.
+
+*/
+
 #include "stopwatch.h"
 
 #if defined (__WIN32__) || defined (_WIN32)
-
 #include <windows.h>
-typedef LARGE_INTEGER* _intern_stopwatch_t;
-
 #else
-
 #include <sys/time.h>
-typedef timeval* _intern_stopwatch_t;
-
-#endif
-
-
-stopwatch_t stopwatch_start() {
-    _intern_stopwatch_t stopwatch = malloc(sizeof(*stopwatch));
-
-    stopwatch_restart(stopwatch);
-
-    return (stopwatch_t) stopwatch;
-}
-
-
-void stopwatch_stop(stopwatch_t stopwatch) {
-    free((_intern_stopwatch_t) stopwatch);
-}
+#end
 
 
 #if defined (__WIN32__) || defined (_WIN32)
 
-void stopwatch_restart(stopwatch_t stopwatch) {
-    QueryPerformanceCounter((LARGE_INTEGER*) stopwatch);
+void stopwatch_start(stopwatch_t *stopwatch) {
+    QueryPerformanceCounter((LARGE_INTEGER*) stopwatch_t);
 }
 
-double stopwatch_gettime(stopwatch_t stopwatch) {
+
+double stopwatch_gettime(stopwatch_t *stopwatch) {
     LARGE_INTEGER frequency;
     LARGE_INTEGER start, stop;
 
     QueryPerformanceFrequency(&frequency);
     QueryPerformanceCounter(&stop);
-    start = * (_intern_stopwatch_t) stopwatch;
+    start = * (LARGE_INTEGER*) stopwatch;
 
     return (stop.QuadPart - start.QuadPart) * 1000.0 / frequency.QuadPart;
 }
 
 #else
 
-void stopwatch_restart(stopwatch_t stopwatch) {
+void stopwatch_start(stopwatch_t *stopwatch) {
     gettimeofday((timeval*) stopwatch, NULL);
 }
 
-double stopwatch_gettime(stopwatch_t stopwatch) {
+
+double stopwatch_gettime(stopwatch_t *stopwatch) {
     timeval start, stop;
 
     gettimeofday(&stop, NULL);
-    start = *(_intern_stopwatch_t) stopwatch;
+    start = *(timeval*) stopwatch;
 
     return (stop.tv_sec - start.tv_sec) * 1000.0 + (stop.tv_usec - start.tv_usec) / 1000.0;
 }
--- stopwatch.h
+++ stopwatch.h
@@ -1,11 +1,53 @@
+/*
+
+    Copyright 2011 Orson Peters. All rights reserved.
+
+    Redistribution of this work, with or without modification, is permitted if
+    Orson Peters is attributed as the original author or licensor of
+    this work, but not in any way that suggests that Orson Peters endorses
+    you or your use of the work.
+
+    This work is provided by Orson Peters "as is" and any express or implied
+    warranties are disclaimed. Orson Peters is not liable for any damage
+    arising in any way out of the use of this work.
+
+*/
+
 #ifndef STOPWATCH_H_GUARD
 #define STOPWATCH_H_GUARD
 
-typedef void* stopwatch_t;
+/* prevent namespace pollution, use own definitions of timeval and LARGE_INTEGER */
 
-stopwatch_t stopwatch_start();
-void stopwatch_restart(stopwatch_t stopwatch);
-double stopwatch_gettime(stopwatch_t stopwatch);
-void stopwatch_stop(stopwatch_t stopwatch);
+#if defined (__WIN32__) || defined (_WIN32)
+
+typedef union {
+    struct {
+        unsigned long LowPart;
+        long HighPart;
+    };
+    struct {
+        unsigned long LowPart;
+        long HighPart;
+    } u;
+
+#if !defined(_M_IX86)
+    long long QuadPart;
+#else
+    double QuadPart;
+#endif
+
+} stopwatch_t;
+
+#else /* defined (__WIN32__) || defined (_WIN32) */
+
+typedef struct {
+    long tv_sec;
+    long tv_usec;
+} stopwatch_t;
+
+#endif
+
+void stopwatch_start(stopwatch_t* stopwatch);
+double stopwatch_gettime(stopwatch_t* stopwatch);
 
 #endif /* STOPWATCH_H_GUARD */
